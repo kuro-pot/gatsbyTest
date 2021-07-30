152b38517c9f74f4c7c435cf5e310746068d93d0
kuro-pot <70866386+kuro-pot@users.noreply.github.com>
2021-07-30 18:28:10 +0900

    公開できるかテスト

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// styles
+
+// markup
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default IndexPage
